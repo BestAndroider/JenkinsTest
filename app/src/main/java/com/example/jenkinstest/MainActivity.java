de33209602aafd8198fbc3cec7baf6c41cae4631
BestAndroider <964862662@qq.com>
2019-02-16 23:58:25 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/jenkinstest/MainActivity.java
+++ app/src/main/java/com/example/jenkinstest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("BASE_URL--------",BuildConfig.BASE_URL);
+        //Log.e("BASE_URL--------",BuildConfig.BASE_URL);
     }
 }
